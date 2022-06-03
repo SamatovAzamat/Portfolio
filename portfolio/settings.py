c882f5b4b0b07c623d7e75bea9424356f00c7ef6
User <Samatovazamat98@gmail.com>
2022-06-03 20:39:50 +0500

    my last

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,5 +1,6 @@
-import os
+
 from pathlib import Path
+import os
 from django.utils.translation import gettext_lazy as _
 
 
@@ -16,7 +17,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["portfolio24uz.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['portfolio24uz.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,11 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'assets']
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'assets')
+]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
